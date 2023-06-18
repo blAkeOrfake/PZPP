afc659cc7dd939eaec7eec2b7a578090b9ed7183
Artur Pisula <arturpisula11@wp.pl>
2023-06-18 15:29:44 +0200

    Added Columns To Users Table

---
+++ backend/Migrations/20230618130720_AddColumnsToUserTable.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnsToUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactAddress",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactCity",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPostalCode",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxAddress",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxCity",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxPostalCode",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ContactAddress",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ContactCity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPostalCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TaxAddress",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TaxCity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TaxPostalCode",
+                table: "Users");
+        }
+    }
+}
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -10,6 +10,19 @@ public class User
     public int? Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Country { get; set; }
+    public string? Address { get; set; }
+    public string? City { get; set; }
+    public string? PostalCode { get; set; }
+    public string? ContactAddress { get; set; }
+    public string? ContactCity { get; set; }
+    public string? ContactPostalCode { get; set; }
+    public string? TaxAddress { get; set; }
+    public string? TaxCity { get; set; }
+    public string? TaxPostalCode { get; set; }
 
     public static IEnumerable<User> GetUsers()
     {
